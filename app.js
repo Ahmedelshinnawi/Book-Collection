aa56c0c56de9a9656108907da8dadeefeb5d0c38
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-12-07 21:08:52 +0200

    Changing the Port

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const connectDB = require('./server/config/db');
-const cookieParser = require('cookie-parser');
-const mongoStore = require('connect-mongo');
-const session = require('express-session');
-
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const connectDB = require("./server/config/db");
+const cookieParser = require("cookie-parser");
+const mongoStore = require("connect-mongo");
+const session = require("express-session");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(methodOverride('_method'));
-app.use(session({
-    secret: 'keyboard cat',
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    store: mongoStore.create({mongoUrl: process.env.MONGODB_URI}),
-}));
+    store: mongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
-
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
