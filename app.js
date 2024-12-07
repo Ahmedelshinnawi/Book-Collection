6c623a01b938f37734eefb1dad0effe74c602059
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-12-07 21:18:44 +0200

    Changing the Port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoStore = require("connect-mongo");
 const session = require("express-session");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
