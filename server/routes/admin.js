04688f63fb0eca7f9909a198649c7b89ef30d3ef
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-12-09 00:45:13 +0200

    Prettier again

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,231 +1,185 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post')
-const User = require('../models/User')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const Post = require("../models/Post");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const adminLayout = "../views/layouts/admin";
 
-const adminLayout = '../views/layouts/admin';
-
-
-const authMiddleware = (req, res, next ) => {
+const authMiddleware = (req, res, next) => {
   const token = req.cookies.token;
 
-  if(!token) {
-    return res.status(401).json( { message: 'Unauthorized'} );
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
-  } catch(error) {
-    res.status(401).json( { message: 'Unauthorized'} );
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
   }
-}
+};
 
-router.get('/admin', async (req, res) => {
+router.get("/admin", async (req, res) => {
   try {
     const locals = {
-        title: "Admin",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
-      }
+      title: "Admin",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
 
     const data = await Post.find();
-    res.render('admin/index', {locals, layout:adminLayout});
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
-
 });
 
+router.post("/admin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
-
-
-
-router.post('/admin', async (req, res) => {
-    try {
-      
-    const {username, password}= req.body;
-    const user = await User.findOne({username});
-
-    if(!user){
-      return res.status(401).json({message: 'Invalid Username or Password'});
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Username or Password" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordValid){
-      return res.status(401).json({message: 'Invalid Username or Password'});
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Username or Password" });
     }
 
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
-    res.cookie('token', token, {httpOnly: true});
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token, { httpOnly: true });
     // res.status(200).json({message: 'Login Successful'});
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Book Collection",
+      description: "Book Collection API with User Authentication.",
+    };
 
+    const data = await Post.find();
+    res.render("admin/dashboard", {
+      locals,
+      data,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Add Book Entry",
+      description: "Book Collection API with User Authentication.",
+    };
+
+    const data = await Post.find();
+    res.render("admin/add-post", {
+      locals,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/add-post", authMiddleware, async (req, res) => {
+  try {
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body,
+      });
+
+      await Post.create(newPost);
+      res.redirect("/dashboard");
     } catch (error) {
       console.log(error);
     }
-  
-  });
-
-
-router.get('/dashboard', authMiddleware, async (req, res) => {
-
-   try {
-    const locals ={
-      title: 'Book Collection',
-      description: 'Book Collection API with User Authentication.'
-  }
-
-    const data = await Post.find();
-    res.render('admin/dashboard',{
-     locals,
-      data,
-      layout:adminLayout
-    });
-
-   } catch (error) {
-     console.log(error);
-    
-   }
-  
-
-});
-
-
-router.get('/add-post', authMiddleware, async (req, res) => {
-
-  try {
-   const locals ={
-     title: 'Add Book Entry',
-     description: 'Book Collection API with User Authentication.'
- }
-
-   const data = await Post.find();
-   res.render('admin/add-post',{
-    locals,
-    layout:adminLayout
-   });
-
   } catch (error) {
     console.log(error);
-   
   }
- 
-
 });
-
-
-router.post('/add-post', authMiddleware, async (req, res) => {
-
-  try {
-   try {
-    const newPost = new Post({
-      title: req.body.title,
-      body: req.body.body
-    });
-
-    await Post.create(newPost);
-     res.redirect('/dashboard');
-   } catch (error) {
-    console.log(error)
-   }
-
-  } catch (error) {
-    console.log(error);
-   
-  }
-
-});
-router.get('/edit-post/:id', authMiddleware, async (req, res) => {
-
+router.get("/edit-post/:id", authMiddleware, async (req, res) => {
   try {
     const locals = {
       title: "Edit Book Entry",
-      description: "Book Collection API with User Authentication."
-    }
-    const data = await Post.findOne({_id: req.params.id});
+      description: "Book Collection API with User Authentication.",
+    };
+    const data = await Post.findOne({ _id: req.params.id });
 
-    res.render('admin/edit-post',{
+    res.render("admin/edit-post", {
       data,
       layout: adminLayout,
-      locals
-    })
-    
+      locals,
+    });
   } catch (error) {
     console.log(error);
   }
-
 });
 
-router.put('/edit-post/:id', authMiddleware, async (req, res) => {
-
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
   try {
-
     await Post.findByIdAndUpdate(req.params.id, {
       title: req.body.title,
       body: req.body.body,
-      updatedAt: Date.now()
-    })
+      updatedAt: Date.now(),
+    });
 
     res.redirect(`/edit-post/${req.params.id}`);
-    
   } catch (error) {
     console.log(error);
   }
-
 });
 
-
-router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
-
+router.delete("/delete-post/:id", authMiddleware, async (req, res) => {
   try {
-    await Post.deleteOne({_id: req.params.id});
+    await Post.deleteOne({ _id: req.params.id });
 
-    res.redirect('/dashboard')
+    res.redirect("/dashboard");
   } catch (error) {
-
     console.log(error);
-
   }
-
 });
 
-router.post('/register', async(req, res) => {
+router.post("/register", async (req, res) => {
   try {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
 
     try {
-      
-      const user = await User.create({username, password: hashPassword});
-      res.status(201).json({message: 'User created', user});
-
-
+      const user = await User.create({ username, password: hashPassword });
+      // res.status(201).json({ message: "User created", user });
+      res.redirect("/admin");
+      document.querySelector(".status").textContent =
+        "User created, Please login";
     } catch (error) {
-      if(error.code === 11000){
-        return res.status(409).json({message: 'Username already exists'});
+      if (error.code === 11000) {
+        return res.status(409).json({ message: "Username already exists" });
       }
-      res.status(500).json({message: 'Server Error'});
-      
+      res.status(500).json({ message: "Server Error" });
     }
-
-
-
   } catch (error) {
     console.log(error);
   }
-
 });
 
-
-router.get('/logout', (req,res)=> {
-  res.clearCookie('token');
-  res.redirect('/admin');
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/admin");
 });
 
 module.exports = router;
